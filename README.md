82b897498b317cbf7175ab75e9fe5917c047a911
Isis <isislee107@gmail.com>
2024-03-01 09:30:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### To compile java code
+
+```
+javac --source-path src -d bin src/*
+```
+
+### To run java code
+
+```
+java -cp bin App
+```
---
+++ src/FileService.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class FileService {
+
+    public List<String> ReadCSV(String fullPathFilename) {
+        // Task 1 - Read Rush2.csv
+        FileReader fr = 
+        return null;
+    }
+
+    public void writeAsCSV(String pokemons, String fullPathFilename) {
+        // Task 1 - your code here
+
+    }
+}
